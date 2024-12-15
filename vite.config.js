228cdc3d70e63059bdc01f4a23112ea203e5f8f6
Mohamed-Hasan-77 <0mdx3210@gmail.com>
2024-12-16 00:15:32 +0200

    dir update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://cookmails.com/office",
+  base: "./",
   plugins: [react()],
 });
+
+
+
+// base: "https://cookmails.com/office",
+
